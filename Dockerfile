c86f15b1143c3447e04af7d5b72b286185c5ac99
pooyaostvoar <pooya.ostovar@gmail.com>
2025-04-19 10:48:50 +0200

    Add migrations
    This commit add migrations using alembic package. For creating new migration it will compare the model updates with sqlite database.

--- Dockerfile
+++ Dockerfile
@@ -11,5 +11,5 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 
-CMD uvicorn app.main:app --host 0.0.0.0 --port 8000 --reload
+CMD CMD alembic upgrade head && uvicorn app.main:app --host 0.0.0.0 --port 8000 --reload
 
---
+++ alembic/env.py
@@ -0,0 +1,52 @@
+from logging.config import fileConfig
+from sqlalchemy import create_engine
+from sqlalchemy import pool
+from alembic import context
+
+from app.models.book import Book
+from sqlmodel import SQLModel
+import os
+
+# This is the Alembic Config object, which provides
+# access to the values within the .ini file in use.
+config = context.config
+
+# Interpret the config file for Python logging.
+fileConfig(config.config_file_name)
+
+
+target_metadata = SQLModel.metadata
+
+def get_url():
+    return 'sqlite:///./tests.db'
+
+def run_migrations_offline():
+    """Run migrations in 'offline' mode."""
+    url = get_url()
+    context.configure(
+        url=url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+def run_migrations_online():
+    """Run migrations in 'online' mode."""
+    connectable = create_engine(get_url(), poolclass=pool.NullPool)
+
+    with connectable.connect() as connection:
+        context.configure(
+            connection=connection,
+            target_metadata=target_metadata,
+        )
+
+        with context.begin_transaction():
+            context.run_migrations()
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
